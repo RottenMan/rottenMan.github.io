e76aba6ce660c16b1dd72c0b463870cd1febb6e3
RottenMan <1054510164@qq.com>
2020-09-08 18:18:17 +0800

    自动提交

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+###
+ # @Author: 小夜勃
+ # @Date: 2020-09-08 18:17:24
+ # @LastEditTime: 2020-09-08 18:18:04
+ # @小夜勃博客:https://zxq.acs.pw/
+### 
+#! /bin/bash
+git add .
+git commit -m '自动提交'
+git push -u origin blog
+cmd /k
